ec31fd2be2a500c455fe4d6471b0bf9671115f22
CinisArboris <eyver.evm@gmail.com>
2020-12-06 22:13:29 -0400

    add : nuevas soluciones [strings].

--- STRINGS/1024.py
+++ STRINGS/1024.py
@@ -1,40 +1,72 @@
 # -*- coding: utf-8 -*-
+import math
 
-def parte_01():
-	cadena = '!@#$'
+# -------------------------------------------------
+def codificar(cadena, posiciones, orientacion, codigo):
+	# [orientacion] == 0 , derecha
+	# [orientacion] == 1 , izquierda
 	resultado = ''
-	#cad_str = cadena
-	#cad_hex = hex(ord(cad_str))		#string		| str
-	#cad_bin = bin(ord(cad_str))		#str > bin  | str
-	#cad_ord = ord(cad_str)			#str > int  | int
-	
-	
 	for cadA in cadena:
-		varA = cadA			#string
-		varB = ord(cadA)	#decimal
-		varC = chr(varB)	#string
-		print(varA, varB, varC)
-		print ('-'*10)
-		varB = varB + 3		#decimal +3
-		varA = chr(varB)	#string +3
-		print ('>', varB, varA)
-		print ('*'*10)
-	#print ('antes' , cadena)
-	#print ('string     ', cad_str)
-	#print ('hexadecimal', cad_hex)
-	#print ('binario    ', cad_bin)
-	#print ('decimal    ', cad_ord)
-	
-	#print ('*****')
-	#for let in cadena:
-	#	resultado = resultado + str((ord(let) + 3))
-	#print ('resultado', resultado)
+		if (
+			((ord(cadA) >= ord('a')) and (ord(cadA) <= ord('o')))
+			or
+			((ord(cadA) >= ord('p')) and (ord(cadA) <= ord('z')))
+			or
+			((ord(cadA) >= ord('A')) and (ord(cadA) <= ord('O')))
+			or
+			((ord(cadA) >= ord('P')) and (ord(cadA) <= ord('Z')))
+			or
+			codigo
+		):
+			varA = cadA			#string
+			varB = ord(cadA)	#decimal
+			varC = chr(varB)	#string
+			if (orientacion == 0): varB = varB + posiciones	#decimal +3
+			if (orientacion == 1): varB = varB - posiciones	#decimal +3
+			varA = chr(varB)	#string +3
+			resultado = resultado + varA
+		else:
+			resultado = resultado + cadA
 	return resultado
-
-
-def parte_02(cadena):
-	return cadena
-
+# -------------------------------------------------
+def parte_01(cadena):
+	# cada letra mayúscula o minúscula
+	# debe desplazarse tres posiciones hacia la derecha
+	resultado = codificar(cadena, 3, 0, False)
+	
+	# invertirse
+	resultado = resultado[::-1]
+	
+	
+	mitad = int(len(resultado)/2)
+	p1 = resultado[:mitad]
+	p2 = resultado[mitad:]
+	# la mitad en adelante (truncado)
+	# deben moverse una posición a la izquierda
+	p2 = codificar(p2, 1, 1, True)
+	
+	resultado = p1+p2
+	return resultado
 # **************************************************
 # ******************  M  A  I  N
-resultado = parte_02(parte_01())
+inicINPUT = 1
+topeINPUT = int(math.pow(10,4))
+
+entrINPUT = int(input())
+if (entrINPUT < inicINPUT): exit()
+if (entrINPUT > topeINPUT): exit()
+
+inicSTR = 1
+topeSTR = int(math.pow(10,3))
+listaLINEAS = []
+for a in range(entrINPUT):
+	entrSTR = input()
+	if (len(entrSTR) < inicINPUT): exit()
+	if (len(entrSTR) > topeINPUT): exit()
+	listaLINEAS.append(entrSTR)
+
+listaCOD = []
+for linea in listaLINEAS:
+	print (parte_01(linea))
+
+
